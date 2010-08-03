311f7b4ab4b41229a15677f3e3acfb2f2cdbf115
eltokyo <eltokyo@gmail.com>
2010-08-03 17:31:12 -0500

    casi nada solo elimine la cabecera de los archivos

--- src/Agentes/ManejadorAgentes.cpp
+++ src/Agentes/ManejadorAgentes.cpp
@@ -1,9 +1,3 @@
-/* 
- * File:   ManejadorAgentes.cpp
- * Author: fabrizio
- * 
- * Created on 1 de agosto de 2010, 09:41 PM
- */
 
 #include "ManejadorAgentes.hpp"
 
--- src/Agentes/ManejadorAgentes.hpp
+++ src/Agentes/ManejadorAgentes.hpp
@@ -1,9 +1,3 @@
-/* 
- * File:   ManejadorAgentes.hpp
- * Author: fabrizio
- *
- * Created on 1 de agosto de 2010, 09:41 PM
- */
 
 #ifndef MANEJADORAGENTES_HPP
 #define	MANEJADORAGENTES_HPP
