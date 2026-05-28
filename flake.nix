14d83c4661346e6f97dd3b0f1e647562b0f9d6af
fabzter <faboster@gmail.com>
2026-05-27 22:17:03 -0600

    build: drop boostPython from flake.nix

--- flake.nix
+++ flake.nix
@@ -10,18 +10,16 @@
     in {
       packages = forAll (pkgs:
         let
-          boostPython = pkgs.boost.override { enablePython = true; python = pkgs.python311; };
           irrlichtFork = pkgs.callPackage ./nix/irrlicht-fork.nix { darwin = pkgs.darwin; };
-        in { inherit boostPython irrlichtFork; });
+        in { inherit irrlichtFork; });
 
       devShells = forAll (pkgs:
         let
-          boostPython = pkgs.boost.override { enablePython = true; python = pkgs.python311; };
           irrlichtFork = pkgs.callPackage ./nix/irrlicht-fork.nix { darwin = pkgs.darwin; };
         in {
           default = pkgs.mkShell {
-            # add config/overlays to the nixpkgs import above if needed later
-            packages = [ pkgs.cmake pkgs.ninja pkgs.pkg-config pkgs.python311 pkgs.python311.pkgs.pybind11 boostPython irrlichtFork ];
+            packages = [ pkgs.cmake pkgs.ninja pkgs.pkg-config pkgs.python311
+                         pkgs.python311.pkgs.pybind11 irrlichtFork ];
           };
         });
     };
